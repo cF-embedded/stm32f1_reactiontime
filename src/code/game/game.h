4ff526a17cfdd9cddd0ebba85c3e3b981228aed7
cF-embedded <filippocerta3@gmail.com>
2025-04-04 20:28:48 +0200

    Project: add game files

---
+++ src/code/game/game.h
@@ -0,0 +1,14 @@
+/**
+ * @file game.h
+ * @author cF-embedded (cf@embedded.pl)
+ * @brief game class
+ *
+ * @copyright Copyright (c) 2024
+ *
+ */
+ 
+ #ifndef _GAME_H
+ #define _GAME_H
+ 
+ #endif   // __GAME_H
+ 
